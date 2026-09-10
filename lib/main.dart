8cbb87bd42c1fff0ec7615f95806d251488ac267
Mohamed Tarek <mohamed.tarek.ismail0@gmail.com>
2026-09-10 16:37:21 +0300

    refactor(performance): remove app default splash screen and debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Currency Exchange App',
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
       home: const ExchangeRatesListScreen(),
     );
