930aa0052ed3e08211cb13db675d7fa3979f7845
Mohamed Tarek <mohamed.tarek.ismail0@gmail.com>
2026-09-10 19:18:55 +0300

    refactor(di): add connection timeout

--- lib/di/di_app.dart
+++ lib/di/di_app.dart
@@ -4,8 +4,18 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:currency_exchange_tracker/core/network/network_info.dart';
 
+const _connectTimeout = Duration(seconds: 10);
+const _receiveTimeout = Duration(seconds: 10);
+
 Future<void> initAppDependencies(GetIt sl) async {
-  sl.registerLazySingleton(() => Dio());
+  sl.registerLazySingleton(
+    () => Dio(
+      BaseOptions(
+        connectTimeout: _connectTimeout,
+        receiveTimeout: _receiveTimeout,
+      ),
+    ),
+  );
   sl.registerLazySingleton(() => Connectivity());
 
   final sharedPreferences = await SharedPreferences.getInstance();
